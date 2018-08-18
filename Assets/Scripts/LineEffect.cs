8eb9ffa0cdbc9da6d0c6f4bfad0f5d37af9c745b
Adnan-PC\Adnan <aea313@nyu.edu>
2018-08-18 17:23:41 -0400

    no message

--- Assets/Scripts/LineEffect.cs
+++ Assets/Scripts/LineEffect.cs
@@ -5,60 +5,29 @@ using UnityEngine;
 [RequireComponent(typeof(LineRenderer))]
 public class LineEffect : MonoBehaviour
 {
-    public int segments;
-
-	// public float radius;
-	public float lineWidth = .1f;
     LineRenderer line;
+    public float animationDuration = .4f;
 
-    public float spreadSpeed;
-    // public float maxRadius = 2f;
-
-    public float length;
-       
-    void Start ()
+    void Start()
     {
         line = gameObject.GetComponent<LineRenderer>();
-       
-        line.positionCount = (segments + 1);
-        // line.useWorldSpace = false;
-        // CreatePoints ();
     }
 
-	void Update() {
-        // AnimateCircle();
-		// CreatePoints();
-	}
 
-
-    void AnimateCircle() {
-        float t = Time.time;
-
-        // if(radius < maxRadius) {
-        //     radius = spreadSpeed * (t*t*t*t*t*t);
-        // }
+    public void PlayLine(float duration)
+    {
+        animationDuration = duration;
     }
-   
-    // void CreatePoints ()
-    // {
-    //     float x;
-    //     float y;
-    //     float z = 0f;
-       
-    //     float angle = 20f;
-       
-    //     for (int i = 0; i < (segments + 1); i++)
-    //     {
-    //         x = Mathf.Sin (Mathf.Deg2Rad * angle) * radius;
-    //         y = Mathf.Cos (Mathf.Deg2Rad * angle) * radius;
-            
-	//         line.positionCount = (segments + 1);
-    //         line.SetPosition (i,new Vector3(x,y,z) );
-            
-    //         angle += (360f / segments);
-    //     }
 
-	// 	line.startWidth = lineWidth;
-	// 	line.endWidth   = lineWidth;
-    // }
+    IEnumerator AnimateLine()
+    {
+        float t = 0;
+
+        while (t < animationDuration)
+        {
+            t += Time.fixedDeltaTime;
+            // line.stuff
+            yield return new WaitForFixedUpdate();
+        }
+    }
 }
