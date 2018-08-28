311e29bb2691d1182f23f7529dc0f3f063e3e97a
Adnan Agha <aea313@nyu.edu>
2018-08-28 14:12:40 -0400

    - Fixed the build issue

--- Assets/TO BE SORTED/Vibrator.cs
+++ Assets/TO BE SORTED/Vibrator.cs
@@ -2,45 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Vibrator : MonoBehaviour {
+public class Vibrator : MonoBehaviour
+{
 
-	public static bool vibeEnabled;
+    public static bool vibeEnabled;
 
-	void Start() {
-		EventManager.StartListening("BallCaught", HandleBallCaught);
-		InitRumble();
-	}
+    void Start()
+    {
+        // EventManager.StartListening("BallCaught", HandleBallCaught);
+        InitRumble();
+    }
 
-	public static void Vibrate() {
-		Handheld.Vibrate();
-	}
+    public static void Vibrate()
+    {
+        Handheld.Vibrate();
+    }
 
-	void HandleBallCaught() {
-		StartCoroutine(Vibrate(.05f));
-	}
+    void HandleBallCaught()
+    {
+        StartCoroutine(Vibrate(.05f));
+    }
 
-	public static IEnumerator Vibrate(float d) {
-		float t = 0;
-		while (t < d) {
-			t += Time.fixedDeltaTime;
-			Handheld.Vibrate();
-			yield return new WaitForFixedUpdate();
-		}
-	}
+    public static IEnumerator Vibrate(float d)
+    {
+        float t = 0;
+        while (t < d)
+        {
+            t += Time.fixedDeltaTime;
+            Handheld.Vibrate();
+            yield return new WaitForFixedUpdate();
+        }
+    }
 
-	public void ToggleRumble() {
-		vibeEnabled = !vibeEnabled;
-	}
+    public void ToggleRumble()
+    {
+        vibeEnabled = !vibeEnabled;
+    }
 
-			public static bool rumbleOn;
-	string rumbleKey = "rumble";
+    public static bool rumbleOn;
+    string rumbleKey = "rumble";
 
-	void InitRumble() {
-		if (!PlayerPrefs.HasKey(rumbleKey)) {
-			PlayerPrefs.SetInt(rumbleKey, 0);
-		} else {
-			rumbleOn = PlayerPrefs.GetInt(rumbleKey) == 1;
-		}
-		vibeEnabled = rumbleOn;
-	}
+    void InitRumble()
+    {
+        if (!PlayerPrefs.HasKey(rumbleKey))
+        {
+            PlayerPrefs.SetInt(rumbleKey, 0);
+        }
+        else
+        {
+            rumbleOn = PlayerPrefs.GetInt(rumbleKey) == 1;
+        }
+        vibeEnabled = rumbleOn;
+    }
 }
