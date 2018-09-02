3008dad6b5df51cd9fd89418e6f7bec7c34cb474
Adnan Agha <aea313@nyu.edu>
2018-09-01 22:21:29 -0400

    - Fixed new ball mask showing up during game over

---
+++ Assets/GameOverStacker.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverStacker : MonoBehaviour {
+
+	public SpriteRenderer dot;
+
+	public int circleCount = 5;
+	public Extensions.Property circleRadius;
+
+
+	IEnumerator StackCircles() {
+
+		for(int i = 0; i < circleCount; i++) {
+			yield return StartCoroutine(SpawnCircle(duration));
+		}
+
+	}
+
+	public float duration;
+
+	IEnumerator SpawnCircle(float d) {
+			float t = 0;
+			d = 1;
+
+			SpriteRenderer s = Instantiate (dot);
+
+			while(t < d) {	
+				t += Time.fixedDeltaTime;
+				float percent = t / d;
+
+				s.transform.localScale = Vector2.one * GetScale(percent);
+
+				yield return new WaitForFixedUpdate();
+			}
+	}
+
+	float GetScale(float t) {
+		return EZEasings.SmoothStart4(t);
+	}
+}
